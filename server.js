61059e023c49b2215e9d5f1685cf0fd36d48500a
adham bishr <adhambishr@gmail.com>
2018-01-13 14:48:38 -0800

    working through deployment

--- server.js
+++ server.js
@@ -8,8 +8,16 @@ const PORT = process.env.PORT || 3001;
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Serve up static assets
-app.use(express.static("client/build"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Add routes, both API and view
 app.use(routes);
 
