4168e8ba77f3e49b36a5c2010e479443d97b8ada
adham bishr <adhambishr@gmail.com>
2018-01-18 18:12:41 -0800

    setting up for heroku

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 // Configure body parser for AJAX requests
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.set("port", process.env.PORT || 3001);
 
 // Add routes, both API and view
 app.use(routes);
@@ -16,10 +17,10 @@ app.use(routes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+  // const path = require("path");
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  // });
 }
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
@@ -31,6 +32,6 @@ mongoose.connect(MONGODB_URI, {
 });
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(app.get("port"), function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
