e04bb27ecbb66959797d5f6e130536deee329b49
adham bishr <adhambishr@gmail.com>
2018-01-18 18:18:59 -0800

    deploying to heroku

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ mongoose.connect(MONGODB_URI, {
 
 // Start the API server
 app.listen(app.get("port"), function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  console.log(`🌎  ==> API Server now listening on PORT ${app.get("port")}!`);
 });
